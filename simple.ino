7f953d8638a8959c0c6b89cac9a34e7619cc6af9
AZAbdullah <annuarzainiabdullah68@gmail.com>
2019-11-01 00:57:46 +0800

    2

--- simple.ino
+++ simple.ino
@@ -2,7 +2,15 @@
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+	Serial.begin(912600, SERIAL_8N1, 3, 1);
+	delay(300);
+	log_i("\n\n\n\nSalam Dunia dari %s\n\n\n\n", __FILE__);
+	log_i("Memory = %d", String(esp_get_free_heap_size()).c_str());
+
+	//initial program load Memory = 1073447996
+
+
+
 }
 
 // The loop function is called in an endless loop
